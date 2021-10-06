903650df1e216c7e7bf9354eafb5dd1f4475804c
SergioAnes <90785004+SergioAnes@users.noreply.github.com>
2021-10-06 08:47:06 +0200

    Add files via upload

--- Ejercicio9/practica-05-09.js
+++ Ejercicio9/practica-05-09.js
@@ -15,19 +15,47 @@ function compruebaEmail() {
 
             esEmail=false
         }
+    
+    var posicionArroba = email.indexOf("@"); //devuelve la posicion del arroba
 
-    else if (!email.includes(arroba)) {
-            
-            esEmail=false
+    for (var i=1; i<posicionArroba; i++) {
 
-    }  
+    if ((email.charAt(i) < "a" || email.charAt(i)>"z") && 
+                !especiales.includes(email.charAt(i)) && 
+                (email.charAt(i)<"0" || email.charAt(i) > "9")  
+                && !arroba.includes(email.charAt(i))) {
 
-    else if (!email.includes(punto)) {
-             esEmail=false
+                esEmail=false;
+        }
 
     }
 
+    var emailPunto = email.substring(posicionArroba+1, email.lastIndexOf("."));
+    var posicionPunto = emailPunto.lastIndexOf("."); //o last index
 
+    for (var i=0; i<posicionPunto; i++) {
+
+    if ((emailPunto.charAt(i) < "a" || emailPunto.charAt(i)>"z") && 
+                !especiales.includes(emailPunto.charAt(i)) && 
+                (emailPunto.charAt(i)<"0" || emailPunto.charAt(i) > "9")){
+
+                esEmail=false;
+        }
+
+    }
+
+    var emailFinal=email.substring(posicionPunto+1, email.length-1);
+    var posicionFinal=emailFinal.length-1;
+
+    for (var i= 0; i<posicionFinal; i++) {
+
+    if ((emailFinal.charAt(i)<"a" || emailFinal.charAt(i>"z")) &&
+            (emailFinal.length < 2 || emailFinal.length > 4)) {
+
+            esEmail=false;
+        }
+    }
+    
     if (esEmail)
        document.formulario.mensaje.value="El email es correcto";
     else 
